d98b022ed93f8b78f7abe35557da08062390c3ba
Jesse David <jesse.p.david@gmail.com>
2021-06-10 11:03:51 -0500

    Install Streaming Monitor

--- orgInit.sh
+++ orgInit.sh
@@ -2,3 +2,6 @@ sfdx force:org:create -f config/project-scratch-def.json -s -d 30 -w 60
 sfdx force:source:push
 sfdx force:user:password:generate
 sfdx force:org:open
+
+# Install Streaming Monitor
+sfdx force:package:install --package 04t1t000003DLAQAA4 -w 2
