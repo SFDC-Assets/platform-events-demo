a33c18a32dd8135060ccf69f2d84f003bac7a98e
Jonathan Zhou <34146693+zhou059@users.noreply.github.com>
2022-03-02 12:54:59 -0800

    Update orgInit.sh

--- orgInit.sh
+++ orgInit.sh
@@ -1,4 +1,4 @@
-sfdx force:org:create -f config/project-scratch-def.json -d 3 -s --wait 60
+sfdx shane:org:create -f config/project-scratch-def.json -d 3 -s --wait 60 --userprefix platform -o events.demo
 sfdx force:source:push
 sfdx shane:user:password:set -g User -l User -p salesforce1
 sfdx force:org:open
